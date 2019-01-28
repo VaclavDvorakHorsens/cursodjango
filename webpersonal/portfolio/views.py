c6757f1d1a5e7ed426e486066c9b3a15537ee6a2
Alias111 <Alias_one11@outlook.com>
2019-01-28 20:36:15 +0100

    Initial commit

---
+++ webpersonal/portfolio/views.py
@@ -0,0 +1,8 @@
+#portfolio/views.py#
+
+from django.shortcuts import render
+from .models import Project
+
+def portfolio(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/portfolio.html', {'projects':projects})
